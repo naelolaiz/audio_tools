ed3bf9df441555d85cb4b8a8d5e489a983da678a
nael <naelblog@gmail.com>
2020-11-01 10:01:34 +0100

    multichannel enabled

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sndfile
 import sys
-#import scipy.signal
 from scipy.signal import fftconvolve
 
 def main() : 
@@ -14,9 +13,10 @@ def main() :
     max_offset = int(sys.argv[3]) if len(sys.argv) == 4 else 0
     audiofile1 = sndfile.open(filename1)
     audiofile2 = sndfile.open(filename2)
-    if audiofile1.channels != 1 or audiofile2.channels !=1 : 
-        print("Only mono files allowed at the moment")
+    if audiofile1.channels != audiofile2.channels : 
+        print("Channel count must match")
         sys.exit(-2)
+    channelCount = audiofile1.channels
 
     toReadFromAudio1 = min(audiofile1.frames, max_offset*2) if max_offset != 0 else audiofile1.frames
     toReadFromAudio2 = min(audiofile2.frames, max_offset*2) if max_offset != 0 else audiofile2.frames
@@ -24,15 +24,16 @@ def main() :
     print("Reading %i frames from %s..." % (toReadFromAudio1, filename1))
     signal1 = audiofile1.read_frames("f", toReadFromAudio1)
     print("Reading %i frames from %s..." % (toReadFromAudio2, filename2))
-    signal2 = audiofile2.read_frames("f", toReadFromAudio2)
-    signal2_reverse = signal2[::-1]
+    signal2_reverse = audiofile2.read_frames("f", toReadFromAudio2)[::-1]
 
-    conv = abs(fftconvolve(signal1, signal2_reverse,  mode="full"))
+    for channel in range(channelCount) :
+        channelSignal1 = [f for (i,f) in enumerate(signal1) if i % channelCount == channel]
+        channelSignal2Reverse = [f for (i,f) in enumerate(signal2_reverse) if i % channelCount == channel]
 
-    #open("conv_out.txt","w").write("\n".join(["%i: %f" % (i,f) for (i,f) in enumerate(conv)]))
+        conv = abs(fftconvolve(channelSignal1,  channelSignal2Reverse,  mode="full"))
+        #open("conv_out.txt","w").write("\n".join(["%i: %f" % (i,f) for (i,f) in enumerate(conv)]))
 
-    print("Max value index : " + str(toReadFromAudio2 - conv.argmax()))
-
+        print("Max value index  for channel %i:  %i" % (channel, (toReadFromAudio2 - conv.argmax())))
 
 if __name__ == "__main__" : main()
 
